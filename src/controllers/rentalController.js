125688584881b96e6faa69a72e5d3ae707adbdac
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-07-24 19:50:10 -0300

    implemnts delete

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -1,36 +1,55 @@
 import connection from '../dbStrategy/postgres.js';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 export async function getRental(req, res){
     try {
-       const { rows: games } = await connection.query('SELECT * FROM games');
+       const { rows: games } = await connection.query('SELECT * FROM rentals');
        res.send(games);
 
      //- Para a rota `/games?name=ba`, deve ser retornado uma array somente com os jogos que comecem com "ba", como "Banco Imobiliário", "Batalha Naval", etc
     } catch (error) {
         res.status(500).send("Erro no servidor")
     }
-    
 }
 
 export async function postRental(req, res){
-    const newGame = req.body
-    const entrySchema = joi.object({
-        name: joi.string().required(),
-        stockTotal: joi.number().required(),
-        pricePerDay: joi.number().required()
+    const newRental = req.body
+    const rentalSchema = joi.object({
+        customerId: joi.number().required(),
+        gameId: joi.number().required(),
+        daysRented: joi.string().required()
       });
-      const { error } = entrySchema.validate(entry);
+     const { error } = rentalSchema.validate(newRental);
       if (error) {
         return res.sendStatus(400);
       }
-    //- não pode ser nome que já existe
+      const day = dayjs().format("YYYY-MM-DD");
+    //   const{ rows: priceGame } = await connection.query(`SELECT games.pricePerDay FROM games`)
+    //   console.log(priceGame.pricePerDay)
+    //   const priceRental = (parseInt(priceGame.pricePerDay) * parseInt(newRental.daysRented))
+    //   console.log(priceRental)
+  
     try {
-        res.sedStatus(201)
-        await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryID", "pricePerDay") VALUES ($1, $2, $3, $4, $5)', [newGame.name, newGame.image, newGame.stockTotal, newGame.categoryId, newGame.pricePerDay])
-        res.Status(201).send("Game novo inserido")
+        const teste = await connection.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice") VALUES ('${newRental.customerId}', '${newRental.gameId}', '${day}', '${newRental.daysRented}', 1000)`)
+       
+        res.status(201).send("Game novo inserido")
     } catch (error) {
+        console.log(error)
         res.status(500).send("Erro no servidor")
     }
 
+}
+
+export async function deleteRental(req, res){
+    const {id} = req.params;
+    const rental = await connection.query(`SELECT id FROM rentals`)
+    
+        const rentalId = rental.rows.map(e => e.id)
+        console.log(rentalId)
+        const existGame = rentalId.find(e => e = id)
+        console.log(existGame)
+        if(existGame){
+            await connection.query(`DELETE FROM rentals WHERE id = ${id};`)
+        }
 }
--- src/routes/rentalRouters.js
+++ src/routes/rentalRouters.js
@@ -1,10 +1,10 @@
-import { getRental, postRental } from '../controllers/rentalController.js';
+import { getRental, postRental, deleteRental } from '../controllers/rentalController.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/rental', getRental);
-router.post('/rental', postRental);
-
+router.get('/rentals', getRental);
+router.post('/rentals', postRental);
+router.delete('/rentals/:id', deleteRental);
 
 export default router;
